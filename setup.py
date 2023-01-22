0ae011f5c80e578a89f81f7e7abad9000ae98a01
Praniii <pvashisht_be20@thapar.edu>
2023-01-23 03:57:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,28 +3,27 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.rst").read_text()
 setup(
-  name = 'Topsis-102017069',         # How you named your package folder (MyLib)
-  packages = ['Topsis-102017069'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TOPSIS Package for Multiple Criteria Decision Making',   # Give a short description about your library
-  author = 'PRANVEE VASHISHT',                   # Type in your name
-  author_email = 'pranveevashisht@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/PranveeVashisht/Topsis-102017069',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/PranveeVashisht/Topsis-102017069/archive/refs/tags/v_02.tar.gz',    # I explain this later on
-  keywords = ['Topsis', 'MCDM', 'package'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'Topsis-102017069',         
+  packages = ['Topsis-102017069'],  
+  version = '0.3',     
+  license='MIT',       
+  description = 'TOPSIS Package for Multiple Criteria Decision Making',   
+  author = 'PRANVEE VASHISHT',                   
+  author_email = 'pranveevashisht@gmail.com',    
+  url = 'https://github.com/PranveeVashisht/Topsis-102017069',   
+  download_url = 'https://github.com/PranveeVashisht/Topsis-102017069/archive/refs/tags/v_03.tar.gz',    
+  install_requires=[            
           'pandas',
           'numpy',
       ],
-  # read the contents of your README file
+
 
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',   
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',    
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
